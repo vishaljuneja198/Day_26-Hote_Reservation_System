05a896c1073b6a2579e94ae1fcd4f780c54e36d8
vishaljuneja198 <103586890+vishaljuneja198@users.noreply.github.com>
2022-07-08 20:39:34 +0530

    Update HotelReservation.java

--- src/main/java/com/bridgelabz/HotelReservation.java
+++ src/main/java/com/bridgelabz/HotelReservation.java
@@ -11,28 +11,16 @@ import java.util.stream.Collectors;
 
 public class HotelReservation {
 
-    /**
-     * PROCEDURE ================================================== 1.created one
-     * Arraylist to store hotels 2. method to add hotels to new 3. method to check
-     * number of hotels in list ===================================================
-     */
-
-    /*
-     * 1.created one Arraylist to store hotels
-     */
+   
     static ArrayList<HotelDetails> hotelList = new ArrayList<HotelDetails>();
 
-    /*
-     * 2. method to add hotels to new
-     */
+   
     public void addNewHotel(String name, int price) {
         HotelDetails temp = new HotelDetails(name, price);
         hotelList.add(temp);
     }
 
-    /*
-     * 3. method to check number of hotels in list
-     */
+    
     public int countNoOfHotels() {
         return hotelList.size();
 
